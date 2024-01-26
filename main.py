ec0645095fab963b074c840593cf98b53e0598d9
zhangz <zishengvc@gmail.com>
2024-01-26 23:22:47 +0800

    cleanup the code in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import importlib
 import os
 from datetime import date, timezone
-from typing import Any
+from typing import Any, Callable
 
 import numpy as np
 from pandas import DataFrame, Timedelta, Timestamp, merge
@@ -15,6 +16,9 @@ from utils.var import ANNUAL_MS, CFG_DIR, INTERVAL_MS_MAP
 
 
 def main(cfg: dict):
+    """
+    1. get sdate, edate
+    """
     # when we load config yaml file to python dict,
     # yyyy-mm-dd format in yaml file will be converted automatically to datetime.date
     # so here they are datetime.date object rather than string
@@ -26,6 +30,9 @@ def main(cfg: dict):
     logger.info(msg=f"Backtesting start time: {s_ts}")
     logger.info(msg=f"Backtesting end time: {e_ts}")
 
+    """
+    2. get timeframe
+    """
     # get timeframe sting from config and convert it to pandas Timedelta object
     timeframe: str = cfg["timeframe"]
     timeframe_ts: Timedelta = Timedelta(
@@ -34,10 +41,16 @@ def main(cfg: dict):
     )
     logger.info(msg=f"Backtesting timeframe: {timeframe_ts}")
 
+    """
+    3. get symbol
+    """
     # get ccxt_sym string from config
     ccxt_sym: str = cfg["ccxt_sym"]
     logger.info(msg=f"Backtesting symbol: {ccxt_sym}")
 
+    """
+    4. load historical price
+    """
     # load price for okx
     logger.info(msg="Loading price for okx")
     okx_prc: DataFrame = load_price(
@@ -46,6 +59,10 @@ def main(cfg: dict):
         exchange="okx",
         symbols=[ccxt_sym],
         timeframe=timeframe,
+    )
+    check_cols(
+        df=okx_prc,
+        cols=["sym", "ts", "open", "high", "low", "close", "volume"],
     )
 
     # load price for binance
@@ -57,19 +74,33 @@ def main(cfg: dict):
         symbols=[ccxt_sym],
         timeframe=timeframe,
     )
+    check_cols(
+        df=binance_prc,
+        cols=["sym", "ts", "open", "high", "low", "close", "volume"],
+    )
 
+    """
+    5. merge dataframe
+    """
     # Data process
-    logger.info(msg="Calculating signal")
     # calculate return
     okx_prc["ret"] = okx_prc["close"] / okx_prc["open"] - 1
     binance_prc["ret"] = binance_prc["close"] / binance_prc["open"] - 1
     # merge data
     prc_df: DataFrame = merge(
-        left=okx_prc[["sym", "ts", "open", "ret"]].rename(
-            columns={"open": "okx_open_prc", "ret": "okx_ret"}
+        left=okx_prc[["sym", "ts", "open", "ret", "volume"]].rename(
+            columns={
+                "open": "okx_open_prc",
+                "ret": "okx_ret",
+                "volume": "okx_vol",
+            }
         ),
-        right=binance_prc[["sym", "ts", "open", "ret"]].rename(
-            columns={"open": "binance_open_prc", "ret": "binance_ret"}
+        right=binance_prc[["sym", "ts", "open", "ret", "volume"]].rename(
+            columns={
+                "open": "binance_open_prc",
+                "ret": "binance_ret",
+                "volume": "binance_vol",
+            }
         ),
         how="inner",
         on=["sym", "ts"],
@@ -83,53 +114,125 @@ def main(cfg: dict):
             "binance_open_prc",
             "okx_ret",
             "binance_ret",
+            "okx_vol",
+            "binance_vol",
         ],
     )
-    # calculate signal
-    prc_df["signal"] = abs((prc_df["binance_open_prc"] / prc_df["okx_open_prc"]) - 1)
-    prc_df["trade"] = prc_df["signal"] > cfg["threshold"]
 
-    # describe signal
-    logger.info(f"Threshold: {cfg['threshold']}")
-    logger.info(f"Min signal: {min(prc_df['signal'])}")
-    logger.info(f"0.25 quantile: {np.percentile(a=prc_df['signal'],q=25)}")
-    logger.info(f"0.75 quantile: {np.percentile(a=prc_df['signal'],q=75)}")
-    logger.info(f"Max signal: {max(prc_df['signal'])}")
-    total_count: int = len(prc_df)
-    signal_triggered_count: int = len(prc_df[prc_df["trade"]])
-    logger.info(f"Opportunities count: {total_count}")
-    logger.info(f"Signal triggered count: {signal_triggered_count}")
-    # count trades
-    if signal_triggered_count / total_count < 0.2:
-        logger.warning(
-            f"Signal was triggered less than 20% of total count: {100*signal_triggered_count/total_count}%, "
-            "please check the threshold."
-        )
-        if signal_triggered_count / total_count == 0:
-            logger.error(f"No signal was triggered, please check the threshold.")
-            return
-
-    # decide side
-    prc_df["binance_side"] = np.select(
-        condlist=[
-            prc_df["trade"] & (prc_df["binance_open_prc"] > prc_df["okx_open_prc"]),
-            prc_df["trade"] & (prc_df["binance_open_prc"] < prc_df["okx_open_prc"]),
+    """
+    6. call custom function to calculate signal and decide trade direction
+    """
+    logger.info(msg="Calculating signal")
+    # call callback function to define binance side and okx side
+    fn: str = cfg["file"]
+    func: str = cfg["func"]
+    module_name: str = "custom." + fn.replace(".py", "")
+    module = importlib.import_module(name=module_name)
+    callback: Callable = getattr(module, func)
+    prc_df: DataFrame = callback(prc_df=prc_df.copy(), cfg=cfg)
+    check_cols(
+        df=prc_df,
+        cols=[
+            "sym",
+            "ts",
+            "okx_open_prc",
+            "binance_open_prc",
+            "okx_ret",
+            "binance_ret",
+            "okx_vol",
+            "binance_vol",
+            "binance_side",
+            "okx_side",
         ],
-        choicelist=[-1, 1],
-        default=0,
+        checkRedundancy=False,
     )
-    prc_df["okx_side"] = -1 * prc_df["binance_side"]
 
+    """
+    7. calculate return, fees
+    """
+    # from the custom function we have defined binance_side and okx_side
     # calculate strategy return at each timepoint
     prc_df["ret"] = (
-        prc_df["binance_side"]
-        * prc_df["binance_ret"]
-        * prc_df["okx_side"]
-        * prc_df["okx_ret"]
+        prc_df["binance_side"] * prc_df["binance_ret"]
+        + prc_df["okx_side"] * prc_df["okx_ret"]
+    )
+    # use previous binance side and current binance side to generate the 'action' column
+    prc_df["prev_binance_side"] = prc_df["binance_side"].shift().fillna(0)
+    prc_df["action"] = np.select(
+        condlist=[
+            # keep same direction
+            prc_df["binance_side"] == prc_df["prev_binance_side"],
+            # close position
+            (prc_df["binance_side"] != prc_df["prev_binance_side"])
+            & (prc_df["binance_side"] == 0),
+            # open position
+            (prc_df["binance_side"] != prc_df["prev_binance_side"])
+            & (prc_df["prev_binance_side"] == 0),
+            # reverse position
+            (prc_df["binance_side"] != prc_df["prev_binance_side"])
+            & ((prc_df["binance_side"] * prc_df["prev_binance_side"]) == -1),
+        ],
+        choicelist=["keep", "close", "open", "reverse"],
+        default=np.nan,
+    )
+    # calculate fees of different tiers
+    prc_df["1bps"] = np.select(
+        condlist=[
+            prc_df["action"].isin(["close", "open"]),
+            prc_df["action"] == "reverse",
+        ],
+        choicelist=[0.0001, 0.0002],
+        default=0,
+    )
+    prc_df["2bps"] = np.select(
+        condlist=[
+            prc_df["action"].isin(["close", "open"]),
+            prc_df["action"] == "reverse",
+        ],
+        choicelist=[0.0002, 0.0004],
+        default=0,
+    )
+    prc_df["3bps"] = np.select(
+        condlist=[
+            prc_df["action"].isin(["close", "open"]),
+            prc_df["action"] == "reverse",
+        ],
+        choicelist=[0.0003, 0.0006],
+        default=0,
+    )
+    # deduct fees from the return to get fee adjusted return
+    prc_df["adj_ret"] = np.select(
+        condlist=[
+            prc_df["action"].isin(["close", "open"]),
+            prc_df["action"] == "reverse",
+        ],
+        choicelist=[
+            prc_df["ret"] - cfg["fee"],
+            prc_df["ret"] - 2 * cfg["fee"],
+        ],
+        default=prc_df["ret"],
     )
 
+    """
+    8. added debug information to be used to improve the performance
+    """
+    # debug columns
+    prc_df["ret_diff"] = prc_df["binance_ret"] - prc_df["okx_ret"]
+    prc_df["ret_diff"] = np.where(
+        prc_df["ret_diff"] > cfg["fee"], prc_df["ret_diff"], 0
+    )
+    prc_df["ideal_binance_side"] = np.select(
+        [prc_df["ret_diff"] > 0, prc_df["ret_diff"] < 0],
+        [1, -1],
+        0,
+    )
+    prc_df.to_csv("debug.csv")
+
+    """
+    9. calculate max drawdown and other risk metrics
+    """
     # caculate max drawdown
-    prc_df["nav"] = (prc_df["ret"] + 1).cumprod()
+    prc_df["nav"] = (prc_df["adj_ret"] + 1).cumprod()
     top_dd: DataFrame = top_drawdown(
         x=np.array(prc_df["nav"]),
         time=np.array(prc_df["ts"]),
@@ -144,18 +247,19 @@ def main(cfg: dict):
 
     # calculate performance metrics
     scalar: float = Timedelta(value=ANNUAL_MS, unit="millisecond") / timeframe_ts
-    annual_ret: float = float(np.mean(a=prc_df["ret"])) * scalar
-    annual_std: float = float(np.std(a=prc_df["ret"], ddof=1) * np.sqrt(scalar))
+    annual_ret: float = float(np.mean(a=prc_df["adj_ret"])) * scalar
+    annual_std: float = float(np.std(a=prc_df["adj_ret"], ddof=1) * np.sqrt(scalar))
     annual_sr: float = annual_ret / annual_std
-    win_ratio: float = len(prc_df[prc_df["ret"] > 0]) / len(prc_df[prc_df["ret"] != 0])
     performance_data: dict = {
         "Annual Return": str(round(annual_ret * 100, 2)) + "%",
         "Annual Std": str(round(annual_std * 100, 2)) + "%",
         "Annual Sharpe": round(annual_sr, 2),
-        "Win Ratio": str(round(win_ratio, 2)) + "%",
         "Max Drawdown": top_dd["max_dd"][0],
     }
 
+    """
+    10. generate report
+    """
     # generate report
     report_data: dict[str, DataFrame] = {
         "config": DataFrame(data=cfg.items(), columns=["param", "value"]),
@@ -163,7 +267,9 @@ def main(cfg: dict):
             data=performance_data.items(), columns=["metrics", "value"]
         ),
         "drawdown": top_dd[["peak_time", "trough_time", "recovery_time", "max_dd"]],
-        "ret": prc_df[["ts", "ret"]],
+        "ret": prc_df[["ts", "ret", "adj_ret"]],
+        "signal": prc_df[["ts", "signal"]],
+        "fee": prc_df[["ts", "1bps", "2bps", "3bps"]],
     }
     gen_report(report_data=report_data)
     return
