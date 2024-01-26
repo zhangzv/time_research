5c68a36d0df9a6475785466d9ca1bdafe232b9bc
zhangz <zishengvc@gmail.com>
2024-01-26 23:25:12 +0800

    added a few more plots in the report

--- utils/report.py
+++ utils/report.py
@@ -1,29 +1,37 @@
 import os
 from datetime import datetime
 
+import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.figure import Figure
 from pandas import DataFrame
 
-from utils.plot import plot_line, plot_table
+from utils.plot import plot_dist, plot_line, plot_table
 from utils.valid import check_cols
 from utils.var import REPORT_DIR
 
 
 def gen_report(report_data: dict[str, DataFrame]):
+    # load data
     cfg_df: DataFrame = report_data["config"]
     performance_df: DataFrame = report_data["performance"]
     dd_df: DataFrame = report_data["drawdown"]
     ret_df: DataFrame = report_data["ret"]
+    signal_df: DataFrame = report_data["signal"]
+    fee_df: DataFrame = report_data["fee"]
     check_cols(df=cfg_df, cols=["param", "value"])
     check_cols(df=performance_df, cols=["metrics", "value"])
     check_cols(df=dd_df, cols=["peak_time", "trough_time", "recovery_time", "max_dd"])
-    check_cols(df=ret_df, cols=["ts", "ret"])
+    check_cols(df=ret_df, cols=["ts", "ret", "adj_ret"])
+    check_cols(df=signal_df, cols=["ts", "signal"])
+    check_cols(df=fee_df, cols=["ts", "1bps", "2bps", "3bps"])
 
+    # make sure the dir exists
     if not os.path.exists(path=REPORT_DIR):
         os.makedirs(name=REPORT_DIR)
 
+    # define pdf object
     pdf_fn: str = ".".join(
         [
             f"R{datetime.now().strftime('%Y%m%d.%H%M%S')}",  # runtime
@@ -33,7 +41,6 @@ def gen_report(report_data: dict[str, DataFrame]):
         ]
     )
     pdf_fp: str = os.path.join(REPORT_DIR, pdf_fn)
-
     pdf = PdfPages(filename=pdf_fp)
 
     # config
@@ -67,7 +74,35 @@ def gen_report(report_data: dict[str, DataFrame]):
         _y=["ret"],
         _x_label="time",
         _y_label="return [%]",
-        _title=f"Cumulative Return",
+        _title=f"Cumulative Return Without Fee",
+        _cumulative=True,
+        _to_percentage=True,
+    )
+    pdf.savefig(figure=fig)
+    plt.close(fig=fig)
+
+    # cumulative return
+    fig: Figure = plot_line(
+        _df=ret_df,
+        _x="ts",
+        _y=["adj_ret"],
+        _x_label="time",
+        _y_label="return [%]",
+        _title=f"Cumulative Return With Fee",
+        _cumulative=True,
+        _to_percentage=True,
+    )
+    pdf.savefig(figure=fig)
+    plt.close(fig=fig)
+
+    # cumulative return
+    fig: Figure = plot_line(
+        _df=fee_df,
+        _x="ts",
+        _y=["1bps", "2bps", "3bps"],
+        _x_label="time",
+        _y_label="fee [%]",
+        _title=f"Fee of Different Tier",
         _cumulative=True,
         _to_percentage=True,
     )
@@ -78,12 +113,34 @@ def gen_report(report_data: dict[str, DataFrame]):
     fig: Figure = plot_line(
         _df=ret_df,
         _x="ts",
-        _y=["ret"],
+        _y=["adj_ret"],
         _x_label="time",
         _y_label="return [%]",
         _title=f"Periodical Return",
         _cumulative=False,
         _to_percentage=True,
+    )
+    pdf.savefig(figure=fig)
+    plt.close(fig=fig)
+
+    # return distribution
+    fig: Figure = plot_dist(
+        data=np.array(object=ret_df["adj_ret"] * 100),
+        bins=100,
+        _x_label="return [%]",
+        _y_label="count",
+        _title=f"Return Distribution",
+    )
+    pdf.savefig(figure=fig)
+    plt.close(fig=fig)
+
+    # signal distribution
+    fig: Figure = plot_dist(
+        data=np.array(object=signal_df["signal"]),
+        bins=100,
+        _x_label="signal",
+        _y_label="count",
+        _title=f"Signal Distribution",
     )
     pdf.savefig(figure=fig)
     plt.close(fig=fig)
